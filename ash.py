fbf6621e70d543f87c5aa56a708f48cdc6b53805
Ashish8094 <ashishkumarkv9c@gmail.com>
2025-07-15 19:15:47 +0530

    Create ash.py

---
+++ ash.py
@@ -0,0 +1,23 @@
+print(2|+5)
+
+print(5/2)
+# in this line / sign for giving decimal value 
+
+# if write // then we grt only int value
+
+print(5//2)
+
+print(5**2)  
+# it mean sqr of 5
+
+
+i = 8
+
+i += 2
+print(i)
+
+
+variable = 2+3*5
+print(variable)
+
+# in * or  / dono m preference first 
