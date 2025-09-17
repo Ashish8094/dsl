86327cceb9397931cfa86e2d08d5e303f7ec9c2e
Ashish8094 <ashishkumarkv9c@gmail.com>
2025-09-17 14:52:12 +0530

    Update linklist.cpp

--- linklist.cpp
+++ linklist.cpp
@@ -1,52 +1,56 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct Node{
-    int data;
-    Node * next;
-
-
+struct Node {
+    int rollNo;
+    string name;
+    float marks;
+    Node* next;
 };
-int main(){
-    Node*head = nullptr;
-    Node*temp = nullptr;
-    Node*tail = nullptr;
+
+int main() {
+    Node* head = nullptr;
+    Node* temp = nullptr;
+    Node* tail = nullptr;
 
     int m;
-    cout<<"Enter the nodes "<<endl;
+    cout << "Enter the number of students: ";
+    cin >> m;
 
-    cin>>m;
-
-    cout<<"Enter the num ";
-    for (int i = 1; i < m; i++)
-    {
+    for (int i = 0; i < m; i++) {
         temp = new Node;
-        int x;
-        cin>>x;
-        temp ->data = x;
-        temp ->next = nullptr;
+        cout << "\nEnter details for student " << (i + 1) << ":\n";
 
+        cout << "Roll No: ";
+        cin >> temp->rollNo;
 
-        if (head == nullptr)
-        {
+        cin.ignore();
+
+        cout << "Name: ";
+        getline(cin, temp->name);
+
+        cout << "Marks: ";
+        cin >> temp->marks;
+
+        temp->next = nullptr;
+
+        if (head == nullptr) {
             head = temp;
             tail = temp;
-        }
-        else{
+        } else {
             tail->next = temp;
             tail = temp;
         }
+    }
 
-       
-        
+    cout << "\nStudent List:\n";
+    Node* curr = head;
+    while (curr != nullptr) {
+        cout << "Roll No: " << curr->rollNo 
+             << ", Name: " << curr->name 
+             << ", Marks: " << curr->marks << endl;
+        curr = curr->next;
     }
-    Node*curr = head;
-    while (curr!=nullptr)
-    {
-       cout<< curr->data<<" "; 
-       curr = curr->next;
-    }
-    cout<<endl;
 
     return 0;
 }
